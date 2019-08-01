2aba65406c68cffe53f771b322751803d6e05792
Matheus Henrique Silva <matheushmms@hotmail.com>
2019-08-01 20:54:32 +0000

    Changing application tree and separating routes and controllers

---
+++ src/app/routes/routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+
+// Main route for application
+router.get('/', (req, res) => {
+    res.format({
+        'application/json': () => {
+            res.send({ response: 'API running ok...' });
+        },
+        'text/html': function () {
+            res.render('main', {
+                titlePage: 'API Home',
+                mainTitle: 'API is running (ok)...'
+            });
+        }
+    });
+});
+
+// Auth routes
+
+router.post('/auth/register', authController.register);
+
+// Users routes
+
+// List all Users
+router.get('/users', userController.index);
+// Show one User
+router.get('/users/:id', userController.show);
+// Create User
+router.post('/users', userController.create);
+// Update User
+router.put('/users/:id', userController.update);
+// Delete User
+router.delete('/users/:id', userController.remove);
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './app/views'))
 
-require('./app/controllers/authController')(app);
-
-// Welcome Page
-app.get('/', (req, res) => {
-    res.send('Node API is on Fire!!');
-});
-
+// Import routes in application
+app.use(require('./app/routes/routes'));
 
 app.listen(port, () => {
     console.log('Server running on port ', port);

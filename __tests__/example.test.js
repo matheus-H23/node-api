5d99909901f5608e3b2718189748c00a81989359
Matheus Henrique Silva <matheushmms@hotmail.com>
2020-07-30 23:07:05 -0300

    Adding jest package and creating sample tests

---
+++ __tests__/example.test.js
@@ -0,0 +1,15 @@
+test('Assegura que dois mais dois é igual a 4', () => {
+  expect(2+2).toBe(4);
+});
+
+test('Assegura que dois mais tres é igual a cinco', () => {
+  expect(2+3).toBe(5);
+});
+
+test('Assegura que tres mais tres é igual a 6', () => {
+  expect(3+3).toBe(6);
+});
+
+test('Assegura que cinco mais cinco é igual a 10', () => {
+  expect(5+5).toBe(10);
+});
